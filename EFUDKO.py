73f02220606bd1eebf7b01cd5795c486445d7979
Ерофеев Антон Александрович <AErofeev@ibs.root.ibscorp.ru>
2020-02-19 17:54:13 +0300

    Some minor changes

--- EFUDKO.py
+++ EFUDKO.py
@@ -18,8 +18,8 @@ class Efudko(object):
         password.send_keys("2")
         password.clear()
         password.send_keys("2wsx2WSX")
-        login = self.driver.find_element_by_name('ns_7_CGAH47L00GNRC0I4B42HGB20E4__login')
-        login.click()
+        login_btn = self.driver.find_element_by_name('ns_7_CGAH47L00GNRC0I4B42HGB20E4__login')
+        login_btn.click()
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
             (By.LINK_TEXT,"ППК ЭФЮДКО")))
         EFUDKO_tab = self.driver.find_element_by_link_text('ППК ЭФЮДКО')
@@ -38,8 +38,8 @@ class Efudko(object):
         password.send_keys("2")
         password.clear()
         password.send_keys("2wsx2WSX")
-        login = self.driver.find_element_by_xpath("//input[@name='save']")
-        login.click()
+        login_btn = self.driver.find_element_by_xpath("//input[@name='save']")
+        login_btn.click()
 
     def close(self):
         self.driver.close()
@@ -62,24 +62,19 @@ class Efudko(object):
             (By.LINK_TEXT,"×")))
         close_btn = self.driver.find_element_by_link_text('×')
         close_btn.click()
-        # proc_type = self.driver.find_element_by_xpath("//input[@tabIndex='-32768' and\
-            #  @placeholder='Для вывода списка введите не менее 2х символов']")
-        # proc_type.send_keys('11')
-        # self.driver.execute_script("""var div1 = document.querySelector('div.col-xs-12 * div.selectize-input.items.not-full'); \
-        #     var input = document.querySelector('div.col-xs-12 * div.selectize-input.items.not-full input');\
-        #     var div2 = document.createElement('div'); \
-        #     div2.innerHTML = '1100 - Согласование наименования КО при ее учреждении'; \
-        #     div1.insertBefore(div2, input); div2.setAttribute('class', 'item'); div2.setAttribute('data-value', '1100');\
-        #     div1.setAttribute('class', 'selectize-input items has-options full has-items');""")
-        # self.driver.execute_script("""var procCodeOpt = document.querySelector('select[name="registrationProcedureCode"] option');\
-        #     procCodeOpt.setAttribute('value', '1100');var structDeptOpt = document.querySelector('select[name="structuralDepartment"] option');\
-        #     structDeptOpt.setAttribute('value', '74');var orgOpt = document.querySelector('select[id="organizationNew"] option');\
-        #     orgOpt.setAttribute('value', 'ООО "Костромаселькомбанк"');""")
-        # proc_type.clear()
-        org_name = self.driver.find_element_by_xpath("//input[starts-with(name(@*), 'jQuery')='141']")
-        org_name.send_keys('11')
-        org_name.sendKeys(Keys.DOWN)
-        org_name.sendKeys(Keys.RETURN)
+        WebDriverWait(self.driver, 60).until(EC.element_to_be_clickable(
+            (By.XPATH,"//input[@tabIndex='-32768' and\
+            @placeholder='Для вывода списка введите не менее 2х символов']")))
+        proc_type = self.driver.find_element_by_xpath("//input[@tabIndex='-32768' and\
+            @placeholder='Для вывода списка введите не менее 2х символов']")
+        proc_type.send_keys('11')
+        # org_name = self.driver.find_element_by_xpath("//input[@*[starts-with(., 'jQuery')]]")
+        # return org_name
+        proc_type.send_keys(Keys.DOWN)
+        proc_type.send_keys(Keys.RETURN)
+        proc_type = self.driver.find_element_by_xpath("//input[@tabIndex='-32768' and\
+            @placeholder='Для вывода списка введите не менее 2х символов']")
+        
         WebDriverWait(self.driver, 60).until(EC.presence_of_element_located(
             (By.LINK_TEXT,"×")))
         responsible = self.driver.find_element_by_xpath("//input[@tabIndex='-32768' and\
@@ -94,3 +89,20 @@ if __name__ == '__main__':
     efudko = Efudko()
     efudko.login()
     efudko.close()
+
+
+
+
+
+
+
+# self.driver.execute_script("""var div1 = document.querySelector('div.col-xs-12 * div.selectize-input.items.not-full'); \
+#     var input = document.querySelector('div.col-xs-12 * div.selectize-input.items.not-full input');\
+#     var div2 = document.createElement('div'); \
+#     div2.innerHTML = '1100 - Согласование наименования КО при ее учреждении'; \
+#     div1.insertBefore(div2, input); div2.setAttribute('class', 'item'); div2.setAttribute('data-value', '1100');\
+#     div1.setAttribute('class', 'selectize-input items has-options full has-items');""")
+# self.driver.execute_script("""var procCodeOpt = document.querySelector('select[name="registrationProcedureCode"] option');\
+#     procCodeOpt.setAttribute('value', '1100');var structDeptOpt = document.querySelector('select[name="structuralDepartment"] option');\
+#     structDeptOpt.setAttribute('value', '74');var orgOpt = document.querySelector('select[id="organizationNew"] option');\
+#     orgOpt.setAttribute('value', 'ООО "Костромаселькомбанк"');""")
